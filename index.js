db54c26cd2623c96df4eec0b2f0bb42d6e392f12
ldandoy <ldandoy@overconsulting.net>
2024-10-16 14:17:39 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore, collection, getDocs} from 'firebase/firestore'
+import 'dotenv/config'
+
+console.log('Start du programme v1 !');
+
+const firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const getFactures = async (db) => {
+    const facturesCol = collection(db, 'factures');
+    const facturesSnapshot = await getDocs(facturesCol);
+    const factures = facturesSnapshot.docs.map(doc => ({...doc.data(), id: doc.id}));
+    return factures;
+}
+
+const factures = await getFactures(db)
+
+console.log(factures);
